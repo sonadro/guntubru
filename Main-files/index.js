eecd0705dbba25c7766addec93eb153c2d910ec8
sonadro <sondre.rooth@gmail.com>
2022-10-14 10:12:01 +0200

    Navbar

--- Main-files/index.js
+++ Main-files/index.js
@@ -7,3 +7,20 @@ htmlKode.innerText = tekst;
 const vhKode = document.querySelector("p.virtualHostKode");
 const vhTekst = "<VirtualHost *: 80>\nServerAdmin brukernavn@domene-navn\nServername domene-navn\nServerAlias domene-navn\nDocumentRoot /var/www/domene-navn/html\nErrorLog ${APACHE_LOG_DIR}/error.log\nCustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>";
 vhKode.innerText = vhTekst;
+
+// Knapper
+const knapper = document.querySelector("div.items");
+
+knapper.addEventListener("click", e => {
+    if (e.target.classList.value.includes("setup")) {
+        scrollTo(0, 110);
+    } else if (e.target.classList.value.includes("install")) {
+        scrollTo(0, 3475);
+    } else if (e.target.classList.value.includes("apache")) {
+        scrollTo(0, 14600);
+    } else if (e.target.classList.value.includes("template")) {
+        scrollTo(0, 16220);
+    } else if (e.target.classList.value.includes("upload")) {
+        scrollTo(0, 18970);
+    }
+});
